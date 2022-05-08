f30331b42aa69b26657a94fdc23973fe3227a996
MaySraHandika <dikapayau492@gmail.com>
2022-05-08 23:35:55 +0700

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Assalamualaikum Wr.Wb</h1>
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    font-family: 'Times New Roman', Times, serif;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<template>
+  <div class="text-center">
+    <h1>Assalamualaikum Wr.Wb</h1>
+    <h1>Pilih Juz atau Surah!</h1>
+  </div>
+</template>
